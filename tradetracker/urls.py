6d6b0347953d88a27d0acc7e2f4dbbd49fcc3a92
jahui <j.aa.dahui@gmail.com>
2016-11-19 23:20:18 -0800

    Edit Page
    1) added edit and delete buttons for each table row
    2) added necessary extra urls and views to handle deletions and edits

--- tradetracker/urls.py
+++ tradetracker/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
 	url(r'^view/', views.viewTrades, name='viewTrades'),
 	url(r'^edit/', views.editTrades, name='editTrades'),
 	url(r'^add/', views.addTrades, name='addTrades'),
-	url(r'^save', views.saveTrade, name='saveTrade'),
+	url(r'^save/', views.saveTrade, name='saveTrade'),
+	url(r'^delete/', views.deleteTrade, name='deleteTrade'),
+	url(r'^edit_trade/', views.editSpecificTrade, name='editSpecificTrade'),
+	url(r'^change/', views.changeTrade, name='changeTrade'),
 ]
--- tradetracker/views.py
+++ tradetracker/views.py
@@ -17,7 +17,21 @@ def addTrades(request):
 	return render(request, 'tradetracker/addTrades.html', {})
 
 def editTrades(request):
-	return HttpResponse("You are at the edit page of the trade tracker.")
+	trade_list = Trade.objects.all()
+	total_earnings = 0
+	for trade in trade_list:
+		total_earnings += trade.price * -trade.shares
+	context = {'trade_list' : trade_list, 'total_earnings' : total_earnings}
+	return render(request, 'tradetracker/editTrades.html', context)
+
+def editSpecificTrade(request):
+	selected_trade = Trade.objects.get(pk=request.POST['selectedTrade'])
+	# date needs to be formatted to properly default
+	raw_date = selected_trade.trade_date
+	# comes in format: Nov. 19, 2016, needs to be YYYY-MM-DD
+	format_date = raw_date.isoformat()
+	return render(request, 'tradetracker/editSpecificTrade.html', 
+		{'selected_trade' : selected_trade, 'format_date' : format_date})
 
 def saveTrade(request):
 	ticker = request.POST['ticker']
@@ -31,3 +45,26 @@ def saveTrade(request):
 	else:
 		return render(request, 'tradetracker/addTrades.html', 
 			{'error_message' : "All fields must be filled!"})
+
+def deleteTrade(request):
+	selected_trade = Trade.objects.get(pk=request.POST['selectedTrade'])
+	selected_trade.delete()
+	return HttpResponseRedirect(reverse('tradetracker:viewTrades'))
+
+def changeTrade(request):
+	selected_trade = Trade.objects.get(pk=request.POST['selectedTrade'])
+	ticker = request.POST['ticker']
+	trade_date = request.POST['tradedate']
+	price = request.POST['price']
+	shares = request.POST['shares']
+
+	if ticker and trade_date and price and shares:
+		selected_trade.ticker = ticker
+		selected_trade.price = price
+		selected_trade.shares = shares
+		selected_trade.trade_date = trade_date
+		selected_trade.save()
+		return HttpResponseRedirect(reverse('tradetracker:viewTrades'))
+	else:
+		return render(request, 'tradetracker/editSpecificTrade.html', 
+			{'selected_trade': selected_trade, 'error_message' : "All fields must be filled!"})
